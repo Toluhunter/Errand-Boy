4db097599e8f4ab78917fe9b20f1aa356170b7a5
toluhunter <toluhunter19@gmail.com>
2023-03-04 08:18:42 +0100

    Updated dependencies

--- errand_boy/settings.py
+++ errand_boy/settings.py
@@ -7,13 +7,8 @@ from dotenv import load_dotenv
 BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv(os.path.join(BASE_DIR, "errand_boy", ".env"))
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.getenv("DEBUG") == 'true')
 
 ALLOWED_HOSTS = [
