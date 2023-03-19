00e9c24cbaf740fc039912a3a3b0fa5ab1b63b56
edolor <aghoghomena3@gmail.com>
2023-03-18 23:13:48 -0400

    Created Base Order creation, list and detail views

--- order/views.py
+++ order/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from .serializers import OrderSerializer
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from account.permissions import IsUser
+from rest_framework.generics import GenericAPIView
 
-# Create your views here.
+
+class CreateOrderView(GenericAPIView):
+    """
+    Creation of order by an authenticated user with ROLE=User.
+    """
+    permission_classes = [IsAuthenticated, IsUser]
+
+
+class ListOrderView(GenericAPIView):
+    """
+    Retrieval of **all** orders page by an authenticated user with ROLE=User.
+    """
+    permission_classes = [IsAuthenticated, IsUser]
+
+
+class OrderDetailView(GenericAPIView):
+    """
+    Retrieval of a **single** order by an authenticated user with ROLE=User.
+    """
+    permission_classes = [IsAuthenticated, IsUser]
