11e3966c9372f42777cb00dfe7f6ab5be1805087
edolor <aghoghomena3@gmail.com>
2023-03-23 17:16:07 -0400

    Added order serializer

--- order/serializers.py
+++ order/serializers.py
@@ -1,20 +1,48 @@
 from rest_framework import serializers
 from .models import Order, Item
+from product.serializers import ProductSerializer
 
 
-class OrderSerializer(serializers.ModelSerializer):
+class ItemSerializer(serializers.ModelSerializer):
+    product = ProductSerializer()
 
-    item = serializers.JSONField(required=True)
+    class Meta:
+        model = Item
+        fields = ["amount", "product"]
+
+
+class CreateOrderSerializer(serializers.ModelSerializer):
+    items = ItemSerializer(many=True)
 
     class Meta:
         model = Order
-        exclude = ['owner']
+        fields = ["address", "instruction"]
 
     def create(self, validated_data):
         '''
         Creation of items and order instance
         '''
-        ...
+        owner = self.context["request"].user
+        validated_data["owner"] = owner
 
-    def update(self, instance, validated_data):
-        ...
+        items_data = validated_data.pop("items") # Removing products
+        total_price = 0
+
+        for item in items_data:
+            # Calculate item total price
+            total_price += (item.amount * item.product.price)
+
+        validated_data["total_price"] = total_price
+        order = Order.objects.create(**validated_data)  # Creating order
+
+        # Creating items
+        for item in items_data:
+            total_price = item.amount * item.product.price
+            Item.objects.create(order=order, product=item.product,
+                                total_price=total_price, **item)
+
+        return order
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    pass
