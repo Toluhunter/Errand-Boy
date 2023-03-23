df56d8f1aca4640755a31ec891f48bd19272fb39
toluhunter <toluhunter19@gmail.com>
2023-03-23 16:57:09 +0100

    list food service

--- foodservice/urls.py
+++ foodservice/urls.py
@@ -7,5 +7,6 @@ app_name = "foodservice"
 urlpatterns = [
     path("register/", views.CreateFoodServiceVIew.as_view(), name="register"),
     path("manage/<str:id>/", views.ManageFoodServiceView.as_view(), name="manage"),
+    path("all/", views.ListFoodServiceView.as_view(), name="list"),
     path("<str:id>/", views.RetrieveFoodServiceView.as_view(), name="retrieve")
 ]
