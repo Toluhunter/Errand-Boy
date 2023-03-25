afc099c89bea5e9e0eb6a9893fddb35af1b9c27b
toluhunter <toluhunter19@gmail.com>
2023-03-25 03:24:31 +0100

    Hot Fix Manage url route

--- product/urls.py
+++ product/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path("category/<uuid:id>/", views.ListCategoryView.as_view(), name="categories"),
     path("add-category/", views.AddCategoryView.as_view(), name="add-category"),
     path("all/", views.ListProductView.as_view(), name="products"),
-    path("manage/<uuid:id>/",
+    path("manage/<str:id>/",
          views.ManageProductView.as_view(), name="manage-product"),
     path("add-product/", views.CreateProductView.as_view(), name="add-product"),
     path("delete-category/<uuid:id>/", views.DeleteCategoryView.as_view(),
