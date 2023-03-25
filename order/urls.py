d4e732bf086bb5c8ddc80e1d02778636cda4d389
edolor <aghoghomena3@gmail.com>
2023-03-25 17:19:15 -0400

    Fixed invalid converter

--- order/urls.py
+++ order/urls.py
@@ -10,5 +10,5 @@ app_name = "order"
 urlpatterns = [
     path("create/", CreateOrderView.as_view(), name="create"),
     path("list/", ListOrderView.as_view(), name="list"),
-    path("<UUID:pk>/", OrderDetailView.as_view(), name="detail")
+    path("<uuid:pk>/", OrderDetailView.as_view(), name="detail")
 ]
