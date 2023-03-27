effc63ca9fb8944862025088c1b09eac122641a6
edolor <aghoghomena3@gmail.com>
2023-03-27 16:16:34 -0400

    Completed Item and Order model draft

--- order/models.py
+++ order/models.py
@@ -36,6 +36,7 @@ class Order(models.Model):
     number = models.IntegerField(
         default=get_order_number, unique=True, blank=False, null=False)
     address = models.TextField(null=False, blank=False)
+    instruction = models.TextField(null=False, blank=False)
     status = models.CharField(
         max_length=7,
         choices=statuses,
@@ -48,12 +49,21 @@ class Order(models.Model):
         null=False, blank=False, default=estimate_delivery_cost)
     date_created = models.DateTimeField(default=timezone.now)
 
+    class Meta:
+        ordering = ["-date_created"]
+
+    def __str__(self) -> str:
+        return f"{self.owner.last_name} {self.owner.first_name}--{self.total_price}--{self.date_created}"
+
 
 class Item(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, blank=False)
     amount = models.PositiveSmallIntegerField(null=False, blank=False)
-    product = models.OneToOneField(
+    product = models.ForeignKey(
         to=Product, on_delete=models.CASCADE, null=False, blank=False, related_name="order_item")
     total_price = models.FloatField(null=False, blank=False)  # Read only field
     order = models.ForeignKey(
-        to=Order, related_name="order", on_delete=models.CASCADE)
+        to=Order, related_name="items", on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f"(Qty: {self.amount}), (Product Name: {self.product.name}), (Total Price: {self.total_price}), (Order ID: {self.order.id})"
