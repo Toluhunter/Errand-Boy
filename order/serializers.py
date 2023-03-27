9b305acff8af5e314dbcfabcc6c4805908945c07
edolor <aghoghomena3@gmail.com>
2023-03-27 16:17:28 -0400

    Added create, detail and list serializers for Order

--- order/serializers.py
+++ order/serializers.py
@@ -1,10 +1,19 @@
 from rest_framework import serializers
 from .models import Order, Item
-from product.serializers import ProductIDSerializer
+from product.serializers import ProductIDSerializer, ProductSerializer
 
 
 class ItemSerializer(serializers.ModelSerializer):
+    """Serializer to return product queryset"""
     product = ProductIDSerializer(required=True)
+
+    class Meta:
+        model = Item
+        fields = ["amount", "product"]
+
+class ItemDetailSerializer(serializers.ModelSerializer):
+    """Serializer to fetch all product details"""
+    product = ProductSerializer(required=True)
 
     class Meta:
         model = Item
@@ -16,10 +25,24 @@ class CreateOrderSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Order
-        fields = ["address", "instruction"]
+        fields = ["items", "address", "instruction"]
 
     def validate(self, attrs):
-        
+        items = attrs["items"]
+
+        if not items:
+            raise serializers.ValidationError(
+                {"error": "Order must contain at least an item."})
+
+        product_ids = [item["product"]["product_id"] for item in items]
+        # Using a set to remove duplicates
+        set_product_ids = set(product_ids)
+
+        # Checking for duplicate product ids
+        if len(set_product_ids) != len(product_ids):
+            raise serializers.ValidationError(
+                {"error": "Duplicate product ID in items array."})
+
         return attrs
 
     def create(self, validated_data):
@@ -33,20 +56,35 @@ class CreateOrderSerializer(serializers.ModelSerializer):
         total_price = 0
 
         for item in items_data:
+            product = item["product"]["product"]
             # Calculate item total price
-            total_price += (item.amount * item.product.price)
+            total_price += (item["amount"] * product.price)
 
         validated_data["total_price"] = total_price
         order = Order.objects.create(**validated_data)  # Creating order
 
         # Creating items
         for item in items_data:
-            total_price = item.amount * item.product.price
-            Item.objects.create(order=order, product=item.product,
+            product = item["product"]["product"]
+            total_price = item["amount"] * product.price
+            Item.objects.create(order=order, product=product,
                                 total_price=total_price, amount=item["amount"])
 
         return order
 
 
-class OrderSerializer(serializers.ModelSerializer):
-    pass
+class ListOrderSerializer(serializers.ModelSerializer):
+    items = ItemSerializer(many=True)
+
+    class Meta:
+        model = Order
+        exclude = ["date_created", "owner"]
+
+
+class DetailOrderSerializer(serializers.ModelSerializer):
+    """Serializer for detail view"""
+    items = ItemDetailSerializer(many=True)
+
+    class Meta:
+        model = Order
+        exclude = ["date_created", "owner"]
