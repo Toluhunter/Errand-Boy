f310334c8e8b80f26ef4483fa0f8fb098028038c
edolor <aghoghomena3@gmail.com>
2023-04-01 17:51:41 -0400

    Created Courier Order model

--- courier/models.py
+++ courier/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from order.models import Order
+from account.models import Account
+from uuid import uuid4
+from django.utils import timezone
 
-# Create your models here.
+
+class CourierOrder(models.Model):
+    """Accepted order model"""
+    id = models.UUIDField(default=uuid4, primary_key=True, unique=True)
+    STATUSES = [
+        ["pending", "Pending"],
+        ["success", "Success"]
+        ["failed", "Failed"],
+    ]
+    # Pickup from restaurant status
+    pickup_status = models.CharField(
+        max_length=7, default="pending", blank=False, null=False, choices=STATUSES)
+    # Delivery to user status
+    delivery_status = models.CharField(
+        max_length=7, default="pending", blank=False, null=False, choices=STATUSES)
+
+    courier = models.ForeignKey(
+        Account, on_delete=models.CASCADE, related_name="courier")
+    order = models.ForeignKey(
+        Order, on_delete=models.CASCADE, related_name="order")
+    pay = models.PositiveBigIntegerField(blank=False, null=False)
+    date_created = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        ordering = ["-ordering"]
+
+    def __str__(self):
+        return f"{self.delivery_status} - {self.courier.last_name} {self.courier.first_name} - {self.pay}"
